6db015c80a8ed3852007c8f00f6da5c1ff30d5ff
jonma <jonmadi21@gmail.com>
2025-01-24 15:03:40 +0100

    first commit

---
+++ tasks/views.py
@@ -0,0 +1,138 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from .models import Task
+
+from .forms import TaskForm
+
+# Create your views here.
+
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, "home/signup.html", {"form": UserCreationForm})
+    else:
+
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"]
+                )
+                user.save()
+                login(request, user)
+                return redirect("tasks")
+            except IntegrityError:
+                return render(
+                    request,
+                    "home/signup.html",
+                    {"form": UserCreationForm, "error": "Username already exists."},
+                )
+
+        return render(
+            request,
+            "home/signup.html",
+            {"form": UserCreationForm, "error": "Passwords did not match."},
+        )
+
+
+@login_required
+def tasks(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, "task/tasks.html", {"tasks": tasks})
+
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "task/create_task.html", {"form": TaskForm})
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect("tasks")
+        except ValueError:
+            return render(
+                request,
+                "task/create_task.html",
+                {"form": TaskForm, "error": "Error creating task."},
+            )
+
+
+def home(request):
+    return render(request, "home/home.html")
+
+
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, "home/signin.html", {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is None:
+            return render(
+                request,
+                "home/signin.html",
+                {
+                    "form": AuthenticationForm,
+                    "error": "Username or password is incorrect.",
+                },
+            )
+
+        login(request, user)
+        return redirect("tasks")
+
+
+def findTasks(request):
+    filterTitle = request.GET.get("filterTitle", "")
+
+    tasks = Task.objects.filter(title=filterTitle)
+
+    return render(request, "task/tasks.html", {"tasks": tasks})
+
+
+def manage_tasks(request):
+    if request.method == "POST":
+
+        action = request.POST.get("action")
+        print(action)
+
+        if action and action.startswith("update_"):
+            task_id = action.split("_")[1]
+            print(f"Actualizar tarea con ID: {task_id}")
+            return redirect("updateTask", task_id=task_id)
+
+        elif action == "delete_selected":
+            tasks_to_delete = request.POST.getlist("tasks_to_delete")
+        if tasks_to_delete:
+            Task.objects.filter(
+                id__in=tasks_to_delete
+            ).delete()  # Elimina las tareas seleccionadas
+            print(f"Tareas eliminadas: {tasks_to_delete}")
+            return redirect("AdminTask")
+
+    tasks = Task.objects.all()
+    return render(request, "task/tasksAdmin.html", {"tasks": tasks})
+
+
+def update_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    if request.method == "POST":
+        task.title = request.POST.get("title")
+        task.description = request.POST.get("description")
+        task.save()
+        return redirect("AdminTask")
+    return render(request, "task/update_task.html", {"task": task})
